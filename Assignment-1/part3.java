88915dc75f6781699d4825d6adc72f0bde39b0be
samyaahsan <96796200+samyaahsan@users.noreply.github.com>
2022-06-23 16:20:26 -0400

    working queue and stack

--- Assignment-1/part3.java
+++ Assignment-1/part3.java
@@ -41,7 +41,58 @@ public class part3 {
         }
     }
 
+
+    public class myQueue{
+        List<Integer> q;
+
+        public myQueue(){
+            q = new LinkedList();
+        }
+
+        //enqueue
+        public void enqueue(Integer data){
+            q.add(data);
+        }
+
+        public Integer dequeue(){
+            if (!q.isEmpty()){
+                return q.remove(0);
+            } 
+            return null;      
+        }
+
+        public Integer rear(){
+            return q.get(q.size()-1);
+        }
+
+        public Integer front(){
+            return q.get(0);
+        }
+
+        public Integer size(){
+            return this.q.size();
+        }
+
+        public boolean isEmpty(){
+            return (q.isEmpty());
+        }
+
+    }
+
     public static void main(String[] args){
+
+        part3 temp2 = new part3();
+        myQueue q = temp2.new myQueue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        System.out.println("size of queue: " + q.size());
+        System.out.println("front of queue: " + q.front());
+        System.out.println("rear of queue: " + q.rear());
+        Integer dq = q.dequeue();
+        System.out.println("dequeue item: " + dq);
+
+
         part3 temp = new part3();
         myStack thisStack = temp.new myStack();
         thisStack.push(42);
@@ -50,5 +101,8 @@ public class part3 {
         int popped_value = thisStack.pop();
         System.out.println("popped value: " + popped_value);
         System.out.println("size of stack: " + thisStack.size());
+
+        //queue
+        
     }
 }
